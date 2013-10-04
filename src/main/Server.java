f03d0a0c48307f87a08ad179cf54c00492bb7b0b
William B. Wettersten <wbw20@case.edu>
2013-10-04 00:08:18 -0400

    server accepts and prints

--- src/main/Server.java
+++ src/main/Server.java
@@ -1,16 +1,27 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.PriorityQueue;
 
 import main.models.Request;
 
 public class Server {
 
+    private static final Integer PORT = 5000;
+
     private PriorityQueue<Request> requests;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ServerSocket socket = new ServerSocket(PORT);
         while(true) {
-            //listen
+            Socket connection = socket.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                 String clientSentence = in.readLine();
+                 System.out.println("Received: " + clientSentence);
         }
     }
 }
