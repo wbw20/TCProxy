925c47dac345646f67c2bd19ffa0886aeffe0ab6
William B. Wettersten <wbw20@case.edu>
2013-10-04 20:34:20 -0400

    drop unruly packets

--- src/main/Server.java
+++ src/main/Server.java
@@ -8,18 +8,33 @@ import java.net.Socket;
 import java.util.PriorityQueue;
 
 import main.models.Request;
+import main.util.Util;
 
 public class Server {
 
     private static final Integer PORT = 5000;
+    private static final Integer ERROR_ON_STARTUP = -1;
 
     private PriorityQueue<Request> requests;
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket socket = new ServerSocket(PORT);
+    public static void main(String[] args){
+        ServerSocket socket = null;
+
+        try {
+            socket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(ERROR_ON_STARTUP);
+        }
+
         while(true) {
-            Socket connection = socket.accept();
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            Socket connection;
+            try {
+                connection = socket.accept();
+                new Request(Util.read(new BufferedReader(new InputStreamReader(connection.getInputStream()))));
+            } catch (IOException e) {
+                // swallow, drop packet
+            }
         }
     }
 }
