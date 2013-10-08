476094d08de3f1fa1f486f70dad42a19f0709213
William B. Wettersten <wbw20@case.edu>
2013-10-07 23:33:50 -0400

    write a failing test

---
+++ src/test/util/UtilTest.java
@@ -0,0 +1,16 @@
+package test.util;
+
+import static org.junit.Assert.assertEquals;
+
+import main.util.Util;
+
+import org.junit.Test;
+
+public class UtilTest {
+
+    @Test
+    public void host() {
+        assertEquals("google.com", Util.host("www.google.com"));
+    }
+
+}
