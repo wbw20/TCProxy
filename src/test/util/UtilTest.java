d590f12a2c802fe18e2681f77507bc74e56c8863
William B. Wettersten <wbw20@case.edu>
2013-10-09 20:48:04 -0400

    some tests for new port function

--- src/test/util/UtilTest.java
+++ src/test/util/UtilTest.java
@@ -10,18 +10,36 @@ import org.junit.Test;
 
 public class UtilTest {
 
+    private static final Integer EIGHTY = 80;
+    private static final Integer FOUR_THOUSAND = 4000;
+
     @Test
     public void host() {
         try {
-			assertEquals("www.google.com", Util.host("www.google.com"));
-	        assertEquals("www.google.com", Util.host("http://www.google.com"));
-	        assertEquals("www.mail.google.com", Util.host("www.mail.google.com"));
-	        assertEquals("www.google.com", Util.host("www.google.com/mail"));
-	        assertEquals("www.google.com", Util.host("www.google.com:4000"));
-	        assertEquals("www.google.com", Util.host("www.google.com:4000/news"));
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+            assertEquals("www.google.com", Util.host("www.google.com"));
+            assertEquals("www.google.com", Util.host("http://www.google.com"));
+            assertEquals("www.mail.google.com", Util.host("www.mail.google.com"));
+            assertEquals("www.google.com", Util.host("www.google.com/mail"));
+            assertEquals("www.google.com", Util.host("www.google.com:4000"));
+            assertEquals("www.google.com", Util.host("www.google.com:4000/news"));
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void port() {
+        try {
+            assertEquals(EIGHTY, Util.port("www.google.com"));
+            assertEquals(EIGHTY, Util.port("http://www.google.com"));
+            assertEquals(EIGHTY, Util.port("www.mail.google.com"));
+            assertEquals(EIGHTY, Util.port("www.google.com/mail"));
+            assertEquals(FOUR_THOUSAND, Util.port("www.google.com:4000"));
+            assertEquals(FOUR_THOUSAND, Util.port("www.google.com:4000/news"));
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
